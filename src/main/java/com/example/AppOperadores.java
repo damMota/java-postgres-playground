8985468fd8e40531a6acb8a4def1dca2d9dd2de8
Alessandro D Mota <alessandro.damiani@marinha.mil.br>
2023-08-17 20:08:16 +0000

    operadores jshell

---
+++ src/main/java/com/example/AppOperadores.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class AppOperadores {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+
+        if(a>b)
+            System.out.println("É");
+        else if(a<b)
+            System.out.println("Nao");
+        else
+            System.out.println("Igual");
+
+        boolean teste = a>b;
+    }
+  
+
+}
