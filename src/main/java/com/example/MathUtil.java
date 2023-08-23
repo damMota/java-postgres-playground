53718c417e295b40ffb8e23ade385a6ff6e76233
Alessandro D Mota <alessandro.damiani@marinha.mil.br>
2023-08-23 02:08:30 +0000

    Tdd aula3e4

--- src/main/java/com/example/MathUtil.java
+++ src/main/java/com/example/MathUtil.java
@@ -4,13 +4,37 @@ public class MathUtil {
     static int mdc(int a, int b){
         a=Math.abs(a);
         b=Math.abs(b);
+        
         int maior = Math.max(a, b);
         b = Math.min(a, b);
         a = maior;
+        
+        //P1
         if(b > 0 && a % b == 0)
             return b;
+        
         if(b==0)
             return Math.abs(a);
-        throw new UnsupportedOperationException("Não é possível calcular MDC");
+        
+        return mdc(a-b,b);
+        //throw new UnsupportedOperationException("Não é possível calcular MDC");
+    }
+
+    static int mdc(int ...valores){
+        if(valores == null){
+            throw new NullPointerException("É necessário passar um valor diferente de nulo");
+        }
+        if(valores.length == 0){
+            throw new IllegalArgumentException("É necessário ao menos um valor para calcular mdc");
+        }
+        int a = valores[0];
+        for (int b : valores){
+            a=mdc(a,b);
+        }
+
+ //       for(int i=1;i<valores.length;i++){
+ //           a = mdc(a,valores[i]);
+  //      }
+        return a;
     }
 }
--- src/test/java/com/example/MathUtilTest.java
+++ src/test/java/com/example/MathUtilTest.java
@@ -1,8 +1,15 @@
 package com.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import static com.example.MathUtil.mdc;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import org.junit.jupiter.api.Test;
 
 public class MathUtilTest {
     @Test
@@ -13,7 +20,7 @@ public class MathUtilTest {
         assertEquals(esperado, obtido);
     }
     @Test
-    void testMdcP1Par() {
+    void testMdcP1Par() {   
         int a = 6, b = 2;
         int esperado = 2;
         int obtido = MathUtil.mdc(a, b);
@@ -54,4 +61,92 @@ public class MathUtilTest {
         int obtido = MathUtil.mdc(-a, -b);
         assertEquals(esperado, obtido);
     }
+    @Test
+    void testMdcP8(){
+        int a = 6;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, a);
+        assertEquals(esperado,obtido);
+    }
+    @Test
+    void testMdcP8Negativo(){
+        int a = -6;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(a, a);
+        assertEquals(esperado,obtido);
+    }
+    @Test
+    void testMdcP12DoisPrimos(){
+        int p= 7, a = p;
+        int esperado = p;
+        int obtido = MathUtil.mdc(p, a);
+        assertEquals(esperado,obtido);
+    }
+    @Test
+    void testMdcP12UmPrimos(){
+        int p= 7, a = 2;
+        int esperado = 1;
+        int obtido = MathUtil.mdc(p, a);
+        assertEquals(esperado,obtido);
+    }
+    @Test
+    void testMdcP12PrimoEUm(){
+        int p= 7, a = 1;
+        int esperado = 1;
+        int obtido = MathUtil.mdc(p, a);
+        assertEquals(esperado,obtido);
+    }
+    @Test
+    void testMdcCasoGeral30e12(){
+        int p= 30, a = 12;
+        int esperado = 6;
+        int obtido = MathUtil.mdc(p, a);
+        assertEquals(esperado,obtido);
+    }  
+    @Test
+    void testMdcCasoGeral12e9(){
+        int p= 12, a = 9;
+        int esperado = 3;
+        int obtido = MathUtil.mdc(p, a);
+        assertEquals(esperado,obtido);
+    }      
+    @Test
+    void testMdcP4(){
+        int m = 2, a= 6, b = 3;
+        /* mdc(12,6) = 2*mdc(6,3)
+         * 6 = 2*3
+         * 
+        */
+        int esperado = MathUtil.mdc(m*a,m*b);
+        int obtido = m*MathUtil.mdc(a, b);
+        assertEquals(esperado,obtido);
+    }      
+    @Test
+    void testMdcP9() {
+        int a = 6, b = 3, c=2;
+        int esperado = 3;
+        int obtido = mdc(a, b);
+        assertTrue(mdc(a,mdc(b,c)) == mdc(mdc(a,b),c) && mdc(mdc(a,b),c) == mdc(a,b,c));
+    }
+    @Test
+    void testMdc3Valores() {
+        int a = 12, b = 6, c=4;
+        int esperado = 2;
+        int obtido = mdc(a, b, c);
+        assertEquals(esperado,obtido);
+    }
+    @Test
+    void testMdcNenhumValor() {
+        //var exception = assertThrows(IllegalArgumentException.class, () -> mdc());
+        var exception = assertThrows(IllegalArgumentException.class, MathUtil::mdc);
+
+        System.out.println(exception.getMessage());
+    }
+    @Test
+    void testMdcNull() {
+       var exception = assertThrows(NullPointerException.class, () -> mdc(null));
+       var megEsperado = "É necessário passar um valor diferente de nulo";
+       assertEquals(megEsperado,exception.getMessage());
+    }
+
 }
