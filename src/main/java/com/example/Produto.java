b1a5ece407b134bed9ee5d22b8762cb2a7bb704b
Alessandro D Mota <alessandro.damiani@marinha.mil.br>
2023-08-23 17:21:04 +0000

    Crud falta aula 6

---
+++ src/main/java/com/example/Produto.java
@@ -0,0 +1,34 @@
+package com.example;
+
+public class Produto {
+    int id;
+    String nome;
+    int marcaId;
+    double valor;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getMarcaId() {
+        return marcaId;
+    }
+    public void setMarcaId(int marcaId) {
+        this.marcaId = marcaId;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    
+}
